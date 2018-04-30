2f95e0eaa68b1f5fe2cdc0fb2be484face1662ee
Julio Rodriguez <juliojrg@gmail.com>
2018-04-30 15:09:10 -0400

    Step 4
    Added Submission Model
    Updated tests
    Updated API to persist Model
    Updated view to show response message

--- app/Http/Controllers/SubmissionController.php
+++ app/Http/Controllers/SubmissionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Submission;
 use Illuminate\Http\Request;
 
 class SubmissionController extends Controller
@@ -11,6 +12,35 @@ class SubmissionController extends Controller
             'text'=>"required"
         ]);
         
-        return ['status'=>'ok', 'data'=>$request->input('text')];
+        //lets persist this value in the database
+        $submission = Submission::create([
+            'text'=>$request->input('text')
+        ]);
+    
+        if (!$submission) {
+            return $this->sendResponse("fail", 'Error creating model');
+        }
+        
+        //return created model
+        
+        return $this->sendResponse('ok', 'Submission Saved', $submission);
+    }
+    
+    
+    private function sendResponse($status, $message = null, $data = null)
+    {
+        $response = [
+            "status" => $status
+        ];
+        
+        if ($message) {
+            $response['message'] = $message;
+        }
+        
+        if ($data){
+            $response['data'] = $data;
+        }
+        
+        return json_encode($response);
     }
 }
